81093849aae06213fbfea4108c45919198641555
Unknown <larzabalh@gmail.com>
2017-12-10 00:30:42 -0300

    FUNCION TODO!

--- public/js/gasto/gasto.js
+++ public/js/gasto/gasto.js
@@ -1,44 +1,16 @@
 function init(){
     limpiar();
-    mostrarform(false);
     listar();
-}
-
-//Función mostrar formulario
-function mostrarform(flag)
-{
-    if (flag)
-    {
-        $("#listadoregistros").hide();
-        $("#form_edicion").show();
-        // $("#btnGuardar").prop("disabled",false);
-        // $("#btnagregar").hide();
-    }
-    else
-    {
-        $("#listadoregistros").show();
-        $("#form_edicion").hide();
-        // $("#btnagregar").show();
-    }
 }
 
 function limpiar()
 {
     $("#gasto").val("");
     $("#tipo").val("");
-
-}
-
-function cancelarform()
-{
-    limpiar();
-    listar();
-    mostrarform(false);
 }
 
 function listar()
 {
-  console.log('hola  ');
             Table=$('#tabla_datos').DataTable({
             "aProcessing": true,//Activamos el procesamiento del datatables
             "aServerSide": true,//Paginación y filtrado realizados por el servidor
@@ -54,29 +26,25 @@ function listar()
           columns: [
               { data: 'gasto'},
               { data: 'tipo'},
-              { 'defaultContent': "<button id='editar' type='button' class='editar btn btn-primary'><i class='fa fa-pencil-square-o'></i></button>	<button id='eliminar' type='button' class='eliminar btn btn-danger' data-toggle='modal' data-target='#modalEliminar' ><i class='fa fa-trash-o'></i></button>"}
+              { 'defaultContent': "<button id='editar' type='button' class='editar btn btn-primary' data-toggle='modal' data-target='#modalEditar'><i class='fa fa-pencil-square-o'></i></button>	<button id='eliminar' type='button' class='eliminar btn btn-danger' data-toggle='modal' data-target='#modalEliminar' ><i class='fa fa-trash-o'></i></button>"}
           ],
           "bDestroy": true,
           "iDisplayLength": 10,//Paginación
           "order": [[ 0, "asc" ]]//Ordenar (columna,orden)
-
       });
       editar_data_id('#tabla_datos tbody',Table);
       eliminar_data_id('#tabla_datos tbody',Table);
-
 }
 
 document.getElementById("btnGuardar").addEventListener("click",function(e){
-  console.log('ACA ESTOY')
   e.preventDefault();
 
   var data = {
             'gasto':$('#gasto').val(),
             'tipo': $('#tipo').val()
-  }
+              }
   var url = "http://localhost:8000/configuracion/gasto/crear"
   var token = $('#token').val();
-
 
   $.ajax({
     url: url,
@@ -86,7 +54,6 @@ document.getElementById("btnGuardar").addEventListener("click",function(e){
     dataType: 'json',
     data: data,
     success:function(response){
-      // $('#exito').fadeIn();
       limpiar();
       $("#exito").show('fade')
       document.getElementById('gasto_exito').innerText =response.mensaje.gasto;
@@ -95,8 +62,11 @@ document.getElementById("btnGuardar").addEventListener("click",function(e){
         init();
       },1500);
     },
-    error: function() {
-                  $('#error').fadeIn();
+    error: function(response) {
+              $('#error').modal('show');
+                setTimeout(function(){
+                  $('#error').modal('hide');
+                    },1500);
               }
   });
 });
@@ -111,15 +81,12 @@ var eliminar_data_id = function (tabla_datos, table){
   })
 }
 
-document.getElementById("eliminar-usuario").addEventListener("click",function(e){
-  console.log('eliminando')
+document.getElementById("eliminar").addEventListener("click",function(e){
   e.preventDefault();
-
+  $('#modalEliminar').modal('hide');
   var data = {'id':$('#id_eliminar').val()};
-  console.log(data)
   var url = "http://localhost:8000/configuracion/gasto/eliminar/"+$('#id_eliminar').val()+""
   var token = $('#token_eliminar').val();
-
 
   $.ajax({
     url: url,
@@ -129,44 +96,36 @@ document.getElementById("eliminar-usuario").addEventListener("click",function(e)
     dataType: 'json',
     data: data,
     success:function(response){
-      $("#abrir").hide();
-      limpiar();
-      $("#exito_eliminar").show('fade')
-      document.getElementById('gasto_exito_eliminar').innerText =response.mensaje.gasto;
-      setTimeout(function(){
-        $("#exito_eliminar").hide('fade');
-        $("#abrir").show();
-        init();
-      },1500);
-
+        $('#exito_eliminar').modal('show');
+        limpiar();
+        document.getElementById('gasto_exito_eliminar').innerText =response.mensaje.gasto;
+        setTimeout(function(){
+          $('#exito_eliminar').modal('hide');
+          init();
+        },1500);
       },
     error: function(response) {
-            console.log();
-                  $('#error').modal('show');
-                  // document.getElementById('mensaje_error').innerText =response.message;
-              }
+        $('#error').modal('show');
+          setTimeout(function(){
+            $('#error').modal('hide');
+              },1500);
+        }
   });
 });
 
 // Funciones de Editar
 var editar_data_id = function (tabla_datos, table){
     $(tabla_datos).on("click", "button.editar", function (e){
-    $("#form_edicion").show();
-    $("#abrir").hide();
-    $("#listadoregistros").hide();
     e.preventDefault();
     var data = table.row( $(this).parents("tr") ).data();
-    console.log(data);
     var id=$('#id_edicion').val(data.id),
         gasto=$('#gasto_edicion').val(data.gasto),
         tipo=$("#tipo_edicion").val(data.id_tipo);
   })
 }
 
-  document.getElementById("btnEditar").addEventListener("click",function(e){
-    console.log('ACA ESTOY')
+  document.getElementById("editar").addEventListener("click",function(e){
     e.preventDefault();
-
     var data = {
               'id':$('#id_edicion').val(),
               'gasto':$('#gasto_edicion').val(),
@@ -174,7 +133,6 @@ var editar_data_id = function (tabla_datos, table){
                 }
     var url = "http://localhost:8000/configuracion/gasto/editar/"+$('#id_edicion').val()+""
     var token = $('#token_edit').val();
-
 
     $.ajax({
       url: url,
@@ -184,21 +142,22 @@ var editar_data_id = function (tabla_datos, table){
       dataType: 'json',
       data: data,
       success:function(response){
-        limpiar();
-        $("#exito_edit").show('fade')
-        document.getElementById('gasto_exito_edit').innerText =response.mensaje.gasto;
-        setTimeout(function(){
-          $("#exito_edit").hide('fade');
-          $("#form_edicion").hide();
-          $("#abrir").show();
-          $("#listadoregistros").show();
-          init();
-        },1500);
-
-      },
+                    $("#abrir").hide();
+                    limpiar();
+                    $("#exito_editar").show('fade')
+                    document.getElementById('gasto_exito_editar').innerText =response.mensaje.gasto;
+                    setTimeout(function(){
+                        init();
+                        $("#exito_editar").hide('fade');
+                        $("#abrir").show();
+                      },1500);
+                  },
       error: function() {
                     $('#error').modal('show');
-                }
+                    setTimeout(function(){
+                      $('#error').modal('hide');
+                      },1500);
+                    }
     });
   });
 
--- resources/views/configuracion/gasto.blade.php
+++ resources/views/configuracion/gasto.blade.php
@@ -12,22 +12,24 @@
       				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
       					<span aria-hidden="true">&times;</span>
       				</button>
-      				<h4 class="modal-title" id="myModalLabel">ERROR</h4>
+      				<h4 class="modal-title alert alert-danger" id="myModalLabel">ERROR</h4>
       			</div>
       			<div class="modal-body" id="mensaje_error">
-      				No se pudo ejecutar la consulta!!!
+              <div class="alert alert-danger">
+                No se pudo ejecutar la consulta!!!
+              </div>
       			</div>
       		</div>
       	</div>
       </div>
       {{-- FIN!!! Alarma de BOOTSTRAP --}}
-      <!-- Trigger the modal with a button -->
-      <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" id="abrir"><i class="fa fa-plus" aria-hidden="true"></i> AGREGAR GASTO</button>
 
+
+      <!-- ALTA DE REGISTROS -->
+      <button type="button" class="btn btn-default btn-lg col-lg-offset-5" data-toggle="modal" data-target="#myModal" id="abrir"><i class="fa fa-plus" aria-hidden="true"></i> AGREGAR GASTO</button>
       <!-- Modal -->
       <div id="myModal" class="modal fade" role="dialog">
       <div class="modal-dialog">
-
         <!-- Modal content-->
         <div class="modal-content">
           <div class="modal-header">
@@ -35,14 +37,12 @@
             <h4 class="modal-title">ALTA DE GASTOS</h4>
           </div>
           <div class="modal-body">
-
             {{-- Alarma de BOOTSTRAP --}}
                 <div class="alert alert-success collapse" id="exito">
                   <a id="linkClose" href="#" class="close">&times;</a>
                     Exito! El Gasto: "<strong><span id="gasto_exito"></span></strong>" dado de alta!!!
               </div>
               {{-- FIN!!! Alarma de BOOTSTRAP --}}
-
               <div id="formularioregistros">
                   <div class="panel panel-default">
 
@@ -67,18 +67,17 @@
                               <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
                               <button type="button" class="btn btn-info" data-dismiss="modal"><i class="fa fa-arrow-circle-left"></i> VOLVER</button>
                             </div>
+                            @if (count($errors)>0)
+                              <div class="alert alert-danger">
+                                <ul>
+                                  @foreach ($errors->all() as $error)
+                                    <li>{{$error}}</li>
+                                  @endforeach
+                                </ul>
+                              </div>
+                            @endif
                             </div>
                           </form>
-                          @if (count($errors)>0)
-                            <div class="alert alert-danger">
-                              <ul>
-                                @foreach ($errors->all() as $error)
-                                  <li>{{$error}}</li>
-                                @endforeach
-                              </ul>
-                            </div>
-
-                          @endif
                         </div>
                       <!-- /.panel-body -->
                   </div>
@@ -88,71 +87,26 @@
         </div>
       </div>
 
-
-{{-- FORMULARIO DE EDICION --}}
-<div class="row" id="form_edicion" >
-  <div class="col-lg-6">
-              {{-- Alarma de BOOTSTRAP --}}
-              <div class="alert alert-success collapse" id="exito_edit">
-                  <a id="linkClose_edit" href="#" class="close">&times;</a>
-                    Exito! El Gasto: "<strong><span id="gasto_exito_edit"></span></strong>" se actualizo correctamente!!!
-              </div>
-              {{-- FIN!!! Alarma de BOOTSTRAP --}}
-
-              <div>
-                  <div class="panel panel-default">
-
-                      <div class="panel-body" >
-                        <strong><h3>EDITAR GASTO</h3></strong>
-                          <form name="formulario_edicion" id="" method="POST">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token_edit">
-                            <input type="hidden" name="id" id="id_edicion">
-                            <div >
-                              <label>Nombre:</label>
-                              <input type="text" class="form-control" name="gasto" id="gasto_edicion"maxlength="50" placeholder="Nombre del gasto">
-                            </div>
-                            <div>
-                              <label>Tipo de Gastos:</label><br>
-                              <select class="form-control" name="tipo" id="tipo_edicion">
-                                <option selected></option>
-                                @foreach ($tipos as $key => $value)
-                                  <option value={{$value->id}}>{{$value->tipo}}</option>
-                                @endforeach
-                              </select>
-                            </div>
-
-                            <div class="modal-footer">
-                              <button class="btn btn-primary" type="submit" id="btnEditar"><i class="fa fa-save"></i> Editar</button>
-                              <button type="button" class="btn btn-info" data-dismiss="modal"><i class="fa fa-arrow-circle-left"></i> VOLVER</button>
-                            </div>
-                            </div>
-                          </form>
-                          @if (count($errors)>0)
-                            <div class="alert alert-danger">
-                              <ul>
-                                @foreach ($errors->all() as $error)
-                                  <li>{{$error}}</li>
-                                @endforeach
-                              </ul>
-                            </div>
-
-                          @endif
-                        </div>
-                      <!-- /.panel-body -->
-                  </div>
-                  <!-- /.panel -->
-              </div>
-            </div>
-{{-- FIN FORM EDICION --}}
-
 {{-- ELIMINACION DE REGISTROS --}}
-{{-- Alarma de BOOTSTRAP --}}
-    <div class="alert alert-success collapse col-lg-8" id="exito_eliminar">
-      <a id="linkClose" href="#" class="close">&times;</a>
-        Exito! El Gasto: "<strong><span id="gasto_exito_eliminar"></span></strong>" ha sido eliminado!!!
+    {{-- Alarma de ELIMINACION BOOTSTRAP --}}
+  <div class="modal fade" id="exito_eliminar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <h4 class="modal-title" id="myModalLabel">ELIMINACION DE REGISTROS</h4>
+        </div>
+        <div class="modal-body" id="">
+          <div class="alert alert-success">
+            Exito! El Gasto: "<strong><span id="gasto_exito_eliminar"></span></strong>" ha sido eliminado!!!
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   {{-- FIN!!! Alarma de BOOTSTRAP --}}
-
 <div>
   <form id="frmEliminarUsuario" action="" method="POST">
     <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token_eliminar">
@@ -169,8 +123,8 @@
             ¿Está seguro de eliminar:" <strong id="gasto_eliminar"></strong> "?
           </div>
           <div class="modal-footer">
-            <button type="button" id="eliminar-usuario" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
-            <button type="button" id="eliminar-cancelar" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+            <button type="button" id="eliminar" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
           </div>
         </div>
       </div>
@@ -178,10 +132,64 @@
     <!-- Modal -->
   </form>
 </div>
-
 {{-- FIN ELIMINACION DE REGISTROS --}}
 
+{{-- EDICION DE REGISTROS --}}
+    {{-- Alarma de BOOTSTRAP --}}
+    <div class="alert alert-success collapse col-lg-8" id="exito_editar">
+      <a id="linkClose" href="#" class="close">&times;</a>
+        Exito! El Gasto: "<strong><span id="gasto_exito_editar"></span></strong>" ha sido Actualizado Correctamente!!!
+  </div>
+    {{-- FIN!!! Alarma de BOOTSTRAP --}}
+  <form id="" action="" method="POST">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token_editar">
+    <input type="hidden" id="id_editar" name="id_eliminar" value="">
+    <!-- Modal -->
+    <div class="modal fade" id="modalEditar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="modalEliminarLabel">Edicion</h4>
+          </div>
+          <div class="modal-body">
+            <form name="formulario_edicion" id="" method="POST">
+              <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token_edit">
+              <input type="hidden" name="id" id="id_edicion">
+              <div >
+                <label>Nombre:</label>
+                <input type="text" class="form-control" name="gasto" id="gasto_edicion"maxlength="50" placeholder="Nombre del gasto">
+              </div>
+              <div>
+                <label>Tipo de Gastos:</label><br>
+                <select class="form-control" name="tipo" id="tipo_edicion">
+                  <option selected></option>
+                  @foreach ($tipos as $key => $value)
+                    <option value={{$value->id}}>{{$value->tipo}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="modal-footer">
+                <button type="button" id="editar" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+              </div>
+              </div>
+            </form>
+          </div>
+          @if (count($errors)>0)
+            <div class="alert alert-danger">
+              <ul>
+                @foreach ($errors->all() as $error)
+                  <li>{{$error}}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
+        </div>
+      </div>
+{{-- FIN EDICION DE REGISTROS --}}
 
+{{-- DATATABLE --}}
           <div class="panel-body" id="listadoregistros">
               <div class="col-lg-12">
                   <table id="tabla_datos"  class="table table-striped table-bordered table-condensed table-hover">
